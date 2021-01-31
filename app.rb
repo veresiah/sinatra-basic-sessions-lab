0c3884a82ddfa1661aa67b73456a5d280f7fcab2
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-01-31 15:05:37 -0500

    commit tree bda51c7c7b54e2be8ef6dea2d9d629f7bcd8c5ab

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+    configure do 
+        enable :sessions 
+        set :session_secret, "VDJFGBDFGBHRSIEGREYBGYEFDSFJDSIEWIB"
+    end 
+
+    get '/' do 
+       erb :index  
+    end 
+
+    post '/checkout' do 
+        @session = session 
+        session["item"] = params["item"]
+        erb :checkout
+    end 
+end 
